92d35323c6255c6bdee9795cbfb7646870b7bf69
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-07-11 12:42:13 -0300

    add: primeira aula sobre anatomia

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,8 +1,18 @@
 public class MinhaClasse {
 
-    public static void main(String[] args) {
-        
-        
+    public static void main (String [] args) {
+
+        String primeiroNome = "Vitória";
+        String segundoNome = "Garrucho";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 
     }
 }
